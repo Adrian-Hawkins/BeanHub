7d53c28e894f505fec5ffbe73449b5188712eaa0
AdrianHawkins <Adrian.Hawkins@bbd.co.za>
2024-03-04 16:03:43 +0200

    Fix: Changed instance type

--- ec2.tf
+++ ec2.tf
@@ -49,7 +49,7 @@ resource "aws_key_pair" "deployer" {
 
 resource "aws_instance" "BeanHub" {
     ami           = "ami-0d940f23d527c3ab1"
-    instance_type = "t3.micro"
+    instance_type = "t2.micro"
     key_name      = aws_key_pair.deployer.key_name
     vpc_security_group_ids = [aws_security_group.ec2_security_group.id]
     tags = {
