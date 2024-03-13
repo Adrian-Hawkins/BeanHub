7d762241a74106548f45a242e4a409a3de50f85f
AdrianHawkins <Adrian.Hawkins@bbd.co.za>
2024-03-13 10:26:32 +0200

    fix: removed logger

--- BeanHub.API/src/main/java/com/bean/api/interceptors/AuthInterceptor.java
+++ BeanHub.API/src/main/java/com/bean/api/interceptors/AuthInterceptor.java
@@ -3,19 +3,15 @@ package com.bean.api.interceptors;
 import com.bean.api.util.ValidateToken;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
 
 @Component
 public class AuthInterceptor implements HandlerInterceptor {
 
-    private Logger LOG = LoggerFactory.getLogger(AuthInterceptor.class);
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
         String accessToken = request.getHeader("Authorization");
-        LOG.info("preHandle invoked...{}:{}     {}"+request.getRequestURI(),request.getMethod(), accessToken);
         if (accessToken != null && accessToken.startsWith("Bearer ")) {
             accessToken = accessToken.substring(7);
             boolean isValidToken = ValidateToken.validate(accessToken);
